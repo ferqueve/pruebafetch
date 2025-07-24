46579e65ded53d9278e482ea5e60706f91a68b47
Fernando Quevedo <ferqueve@gmail.com>
2025-07-24 11:31:04 -0300

    Prueba fetch

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const url="https://papayawhip-octopus-853800.hostingersite.com/ciudades.json";
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+       document.getElementById('buscar').addEventListener('click',()=>{
+
+            fetch(url)
+            .then(response => response.json())
+            .then(ciudades => {
+
+                let habitantes= document.getElementById('poblacion').value; 
+                let datos="";
+                for (let ciudad of ciudades ){
+                    if (ciudad.poblacion >= habitantes){
+                        datos+="<tr><td>"+ ciudad.ciudad+"</td><td>"+ ciudad.poblacion +"<td></tr>";
+                    }
+                }
+                    document.getElementById('datos').innerHTML =datos;
+
+
+
+            })
+
+
+
+
+       })
+
+
+
+
+
+
+
+
+})
